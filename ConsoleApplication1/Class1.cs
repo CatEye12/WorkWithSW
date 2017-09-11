0347f3067d022de438c2d0968c046efe23c89acb
CatEye12 <dashulechka20@yandex.ru>
2017-09-11 08:17:01 +0300

    Add project files.

---
+++ ConsoleApplication1/Class1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using SolidWorks.Interop.sldworks;
+using System.Threading;
+using System.Diagnostics;
+using Microsoft.Win32;
+using System.Runtime.InteropServices;
+
+namespace ConsoleApplication1
+{
+    class MyProcess
+    {
+        //C:\Program Files\SOLIDWORKS Corp\SOLIDWORKS\ + 
+
+        //delegate void del();
+
+        // MyProcess instance = new MyProcess();
+        // del d = new del(CheckProcess);
+
+
+        public static int errors;
+        public static int warnings;
+        Program prog = new Program();
+        public SldWorks CheckProcess()
+        {
+            Process[] process = Process.GetProcessesByName("SLDWORKS");
+            Process pr = new Process();
+            string path = null;
+
+           
+            if (process.Length > 0)
+            {
+                Console.WriteLine(process.Length.ToString());
+                Console.WriteLine("The process is already going!");
+                Console.ReadLine();
+            }
+            else
+            {
+                string p = "SOFTWARE\\SolidWorks\\SOLIDWORKS 2016\\Setup\\";
+                
+                RegistryKey key = Registry.LocalMachine.OpenSubKey(p);
+                
+                var installPath = key.GetValue(@"SolidWorks Folder");
+                
+                if (installPath != null)
+                {
+                   path = installPath.ToString() + "SLDWORKS.exe";
+                }
+                else
+                {
+                    Console.Write("Path is null");                    
+                }
+
+                Console.WriteLine("THE");
+                
+                ProcessStartInfo info = new ProcessStartInfo(path);
+                pr.StartInfo = info;
+                pr.Start();
+
+
+
+
+                Thread main = Thread.CurrentThread;
+                Thread.Sleep(5000);
+                // pr.WaitForExit();
+                Console.WriteLine("END");
+                //Console.ReadLine();
+            };
+            return (SldWorks)Marshal.GetActiveObject("SldWorks.Application");            
+        }
+    }
+}
---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,16 @@
+﻿using SolidWorks.Interop.sldworks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {       
+        //delegate void del();
+        static void Main(string[] args)
+        {
+            MyProcess instance = new MyProcess();
+            SldWorks sldWoksApp = null;
+            sldWoksApp = instance.CheckProcess();
+            sldWoksApp.OpenDoc6("D:\\80 K_TOP.SLDPRT", 1, 1, "00", ref MyProcess.errors, ref MyProcess.warnings);
+        }
+    }
+}
